e7d88c61c427369ba09b3850e4b41f3167c4b374
Benedikt Terhechte <terhechte@gmail.com>
2019-04-06 20:00:34 +0200

    Fix macOS bug

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -644,7 +644,7 @@ impl ContextExt for Context {
             let backend = self.backend.borrow();
             if !backend.is_current() {
                 unsafe { backend.make_current() };
-                debug_assert!(backend.is_current());
+
             }
         }
 
